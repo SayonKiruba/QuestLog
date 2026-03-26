0cf22742d529169f39776e3d78d2133b539c10f8
Haseeb Sajjad <Haseebsajjad1718@gmail.com>
2026-03-26 14:45:58 -0400

    Build scripts, unit tests

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo Installing dependencies...
+pip install -r requirements.txt
+
+echo Running app...
+python app.py
+
+pause
---
+++ test.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo Running tests...
+
+python -m unittest
+
+pause
